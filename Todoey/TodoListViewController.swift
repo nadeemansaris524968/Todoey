3faf54f3255fcbe3f6de4f1952a22bce56808582
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-06-25 18:32:38 -0400

    Add table view delegate method to select row and toggle accessory of cell.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -16,6 +16,7 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - Data Source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -24,5 +25,18 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }
+    
+    //MARK: - Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let selectedCell = tableView.cellForRow(at: indexPath) else { return }
+        
+        if selectedCell.accessoryType == .checkmark {
+            selectedCell.accessoryType = .none
+        } else {
+            selectedCell.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
